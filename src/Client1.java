e14af3703c7c129c24f1e5f969e6231b7c4404e0
Akash Chaturvedi <axc144430@utdallas.edu>
2016-01-24 19:40:56 -0600

    added a chat client

--- src/Client1.java
+++ src/Client1.java
@@ -1,9 +1,25 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Client1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException, IOException {
 		// TODO Auto-generated method stub
 		//sample comment from devMayank branch
+		String sentence;
+		String modifiedSentence;
+		BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));   
+		Socket clientSocket = new Socket("localhost", 6789);   
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());   
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));   
+		sentence = inFromUser.readLine();   outToServer.writeBytes(sentence + '\n');   
+		modifiedSentence = inFromServer.readLine();   
+		System.out.println("FROM SERVER: " + modifiedSentence);   
+		clientSocket.close();
 
 	}
 
